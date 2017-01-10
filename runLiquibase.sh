d94b49f09d6bc4af746da97bd7268565dcfeaa23
Kayan Azimov <kayan.aimov@hmcts.net>
2017-01-10 14:21:11 +0000

    Improve performace of deployment by removing redundant vagrant command which triggers ansible scripts for no reason
    Change-Id: I3df74aa03db965cf2efc9338ce4a55aa8705f3ca

--- runLiquibase.sh
+++ runLiquibase.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
+. functions.sh
+
 CONTEXT_NAME=hearing
-mvn -f ${CONTEXT_NAME}-viewstore/${CONTEXT_NAME}-viewstore-liquibase/pom.xml -Dliquibase.url=jdbc:postgresql://localhost:5432/${CONTEXT_NAME}viewstore -Dliquibase.username=${CONTEXT_NAME} -Dliquibase.password=${CONTEXT_NAME} -Dliquibase.logLevel=info resources:resources liquibase:update
+
+runLiquibase
