99579c5ebd4810e640428f3a9f10a318bbdac49b
eltonv.dv2 <elton.vianna@hmcts.net>
2018-04-09 17:07:37 +0100

    GPE-3267 - Fixed unit test
    Change-Id: I689cbf105570ac03f1a554df8645338457a63bdb

--- hearing-command/hearing-command-handler/src/test/java/uk/gov/moj/cpp/hearing/command/handler/NewModelUpdatePleaCommandHandlerTest.java
+++ hearing-command/hearing-command-handler/src/test/java/uk/gov/moj/cpp/hearing/command/handler/NewModelUpdatePleaCommandHandlerTest.java
@@ -1,52 +1,5 @@
 package uk.gov.moj.cpp.hearing.command.handler;
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
-import uk.gov.justice.domain.aggregate.Aggregate;
-import uk.gov.justice.services.common.converter.JsonObjectToObjectConverter;
-import uk.gov.justice.services.common.converter.ObjectToJsonObjectConverter;
-import uk.gov.justice.services.common.converter.jackson.ObjectMapperProducer;
-import uk.gov.justice.services.core.aggregate.AggregateService;
-import uk.gov.justice.services.core.enveloper.Enveloper;
-import uk.gov.justice.services.eventsourcing.source.core.EventSource;
-import uk.gov.justice.services.eventsourcing.source.core.EventStream;
-import uk.gov.justice.services.messaging.JsonEnvelope;
-import uk.gov.moj.cpp.hearing.command.plea.HearingUpdatePleaCommand;
-import uk.gov.moj.cpp.hearing.domain.aggregate.NewModelHearingAggregate;
-import uk.gov.moj.cpp.hearing.domain.aggregate.OffenceAggregate;
-import uk.gov.moj.cpp.hearing.domain.event.CaseAssociated;
-import uk.gov.moj.cpp.hearing.domain.event.ConvictionDateAdded;
-import uk.gov.moj.cpp.hearing.domain.event.ConvictionDateRemoved;
-import uk.gov.moj.cpp.hearing.domain.event.CourtAssigned;
-import uk.gov.moj.cpp.hearing.domain.event.DefenceCounselAdded;
-import uk.gov.moj.cpp.hearing.domain.event.DraftResultSaved;
-import uk.gov.moj.cpp.hearing.domain.event.HearingAdjournDateUpdated;
-import uk.gov.moj.cpp.hearing.domain.event.HearingInitiated;
-import uk.gov.moj.cpp.hearing.domain.event.HearingPleaAdded;
-import uk.gov.moj.cpp.hearing.domain.event.HearingPleaChanged;
-import uk.gov.moj.cpp.hearing.domain.event.HearingPleaUpdated;
-import uk.gov.moj.cpp.hearing.domain.event.HearingVerdictUpdated;
-import uk.gov.moj.cpp.hearing.domain.event.Initiated;
-import uk.gov.moj.cpp.hearing.domain.event.JudgeAssigned;
-import uk.gov.moj.cpp.hearing.domain.event.OffencePleaUpdated;
-import uk.gov.moj.cpp.hearing.domain.event.OffenceVerdictUpdated;
-import uk.gov.moj.cpp.hearing.domain.event.PleaAdded;
-import uk.gov.moj.cpp.hearing.domain.event.PleaChanged;
-import uk.gov.moj.cpp.hearing.domain.event.ProsecutionCounselAdded;
-import uk.gov.moj.cpp.hearing.domain.event.ResultAmended;
-import uk.gov.moj.cpp.hearing.domain.event.ResultsShared;
-import uk.gov.moj.cpp.hearing.domain.event.RoomBooked;
-import uk.gov.moj.cpp.hearing.domain.event.VerdictAdded;
-
-import java.util.UUID;
-import java.util.function.Consumer;
-
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
 import static java.util.UUID.randomUUID;
 import static org.hamcrest.CoreMatchers.allOf;
@@ -63,6 +16,53 @@ import static uk.gov.justice.services.test.utils.core.matchers.JsonEnvelopePaylo
 import static uk.gov.justice.services.test.utils.core.matchers.JsonEnvelopeStreamMatcher.streamContaining;
 import static uk.gov.justice.services.test.utils.core.messaging.JsonEnvelopeBuilder.envelopeFrom;
 import static uk.gov.justice.services.test.utils.core.random.RandomGenerator.PAST_LOCAL_DATE;
+
+import java.util.function.Consumer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import uk.gov.justice.services.common.converter.JsonObjectToObjectConverter;
+import uk.gov.justice.services.common.converter.ObjectToJsonObjectConverter;
+import uk.gov.justice.services.common.converter.jackson.ObjectMapperProducer;
+import uk.gov.justice.services.core.aggregate.AggregateService;
+import uk.gov.justice.services.core.enveloper.Enveloper;
+import uk.gov.justice.services.eventsourcing.source.core.EventSource;
+import uk.gov.justice.services.eventsourcing.source.core.EventStream;
+import uk.gov.justice.services.messaging.JsonEnvelope;
+import uk.gov.moj.cpp.hearing.command.plea.HearingUpdatePleaCommand;
+import uk.gov.moj.cpp.hearing.command.plea.Offence;
+import uk.gov.moj.cpp.hearing.command.plea.Plea;
+import uk.gov.moj.cpp.hearing.domain.aggregate.NewModelHearingAggregate;
+import uk.gov.moj.cpp.hearing.domain.event.CaseAssociated;
+import uk.gov.moj.cpp.hearing.domain.event.ConvictionDateAdded;
+import uk.gov.moj.cpp.hearing.domain.event.ConvictionDateRemoved;
+import uk.gov.moj.cpp.hearing.domain.event.CourtAssigned;
+import uk.gov.moj.cpp.hearing.domain.event.DefenceCounselAdded;
+import uk.gov.moj.cpp.hearing.domain.event.DraftResultSaved;
+import uk.gov.moj.cpp.hearing.domain.event.HearingAdjournDateUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.HearingInitiated;
+import uk.gov.moj.cpp.hearing.domain.event.HearingOffencePleaUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.HearingPleaAdded;
+import uk.gov.moj.cpp.hearing.domain.event.HearingPleaChanged;
+import uk.gov.moj.cpp.hearing.domain.event.HearingPleaUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.HearingVerdictUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.Initiated;
+import uk.gov.moj.cpp.hearing.domain.event.JudgeAssigned;
+import uk.gov.moj.cpp.hearing.domain.event.OffencePleaUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.OffenceVerdictUpdated;
+import uk.gov.moj.cpp.hearing.domain.event.PleaAdded;
+import uk.gov.moj.cpp.hearing.domain.event.PleaChanged;
+import uk.gov.moj.cpp.hearing.domain.event.ProsecutionCounselAdded;
+import uk.gov.moj.cpp.hearing.domain.event.ResultAmended;
+import uk.gov.moj.cpp.hearing.domain.event.ResultsShared;
+import uk.gov.moj.cpp.hearing.domain.event.RoomBooked;
+import uk.gov.moj.cpp.hearing.domain.event.VerdictAdded;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NewModelUpdatePleaCommandHandlerTest {
@@ -87,6 +87,7 @@ public class NewModelUpdatePleaCommandHandlerTest {
             //new events.
             Initiated.class,
 
+            HearingOffencePleaUpdated.class,
             OffencePleaUpdated.class,
             OffenceVerdictUpdated.class,
 
@@ -109,13 +110,10 @@ public class NewModelUpdatePleaCommandHandlerTest {
         setField(this.objectToJsonObjectConverter, "mapper", new ObjectMapperProducer().objectMapper());
     }
 
-    //TODO - handle multiple plea updates in a single call
-
-    @Ignore
-    @Test
+    @Test //TODO - handle multiple plea updates in a single call
     public void updatePlea() throws Throwable {
 
-        HearingUpdatePleaCommand hearingUpdatePleaCommand = HearingUpdatePleaCommand.builder()
+        HearingUpdatePleaCommand command = HearingUpdatePleaCommand.builder()
                 .withCaseId(randomUUID())
                 .withHearingId(randomUUID())
                 .addDefendant(uk.gov.moj.cpp.hearing.command.plea.Defendant.builder()
@@ -132,35 +130,27 @@ public class NewModelUpdatePleaCommandHandlerTest {
                 )
                 .build();
 
-        setupMockedEventStream(hearingUpdatePleaCommand.getDefendants().get(0).getOffences().get(0).getId(), this.offenceEventStream, new NewModelHearingAggregate());
+        when(this.eventSource.getStreamById(command.getHearingId())).thenReturn(this.offenceEventStream);
+        when(this.aggregateService.get(this.offenceEventStream, NewModelHearingAggregate.class)).thenReturn(new NewModelHearingAggregate());
 
-        final JsonEnvelope addPleaCommand = envelopeFrom(metadataWithRandomUUID("hearing.update-plea"), objectToJsonObjectConverter.convert(hearingUpdatePleaCommand));
+        final JsonEnvelope jsonEnvelop = envelopeFrom(metadataWithRandomUUID("hearing.update-plea"), objectToJsonObjectConverter.convert(command));
 
-        this.hearingCommandHandler.updatePlea(addPleaCommand);
+        this.hearingCommandHandler.updatePlea(jsonEnvelop);
 
+        final Offence offence = command.getDefendants().get(0).getOffences().get(0);
+        final Plea plea = offence.getPlea();
+        
         assertThat(verifyAppendAndGetArgumentFrom(this.offenceEventStream), streamContaining(
                 jsonEnvelope(
-                        withMetadataEnvelopedFrom(addPleaCommand)
-                                .withName("hearing.offence-plea-updated"),
+                        withMetadataEnvelopedFrom(jsonEnvelop)
+                                .withName("hearing.hearing-offence-plea-updated"),
                         payloadIsJson(allOf(
-                                withJsonPath("$.originHearingId", equalTo(hearingUpdatePleaCommand.getHearingId().toString())),
-                                withJsonPath("$.offenceId", equalTo(hearingUpdatePleaCommand.getDefendants().get(0).getOffences().get(0).getId().toString())),
-                                withJsonPath("$.pleaDate", equalTo(hearingUpdatePleaCommand.getDefendants().get(0).getOffences().get(0).getPlea().getPleaDate().toString())),
-                                withJsonPath("$.value", equalTo(hearingUpdatePleaCommand.getDefendants().get(0).getOffences().get(0).getPlea().getValue()))
+                                withJsonPath("$.hearingId", equalTo(command.getHearingId().toString())),
+                                withJsonPath("$.offenceId", equalTo(offence.getId().toString())),
+                                withJsonPath("$.pleaDate", equalTo(plea.getPleaDate().toString())),
+                                withJsonPath("$.value", equalTo(plea.getValue()))
                         )))
         ));
-    }
-
-    private <T extends Aggregate> void setupMockedEventStream(UUID id, EventStream eventStream, T aggregate) {
-        setupMockedEventStream(id, eventStream, aggregate, a -> {
-        });
-    }
-
-    private <T extends Aggregate> void setupMockedEventStream(UUID id, EventStream eventStream, T aggregate, Consumer<T> consumer) {
-        consumer.accept(aggregate);
-        when(this.eventSource.getStreamById(id)).thenReturn(eventStream);
-        Class<T> clz = (Class<T>) aggregate.getClass();
-        when(this.aggregateService.get(eventStream, clz)).thenReturn(aggregate);
     }
 
     public static <T> T with(T object, Consumer<T> consumer) {
