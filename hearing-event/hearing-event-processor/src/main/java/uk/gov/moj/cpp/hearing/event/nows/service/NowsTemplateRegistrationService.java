cc0e0c622674d772d73fa214edcba711f91a54fc
Deepak Halale <deepak.halale@hmcts.net>
2018-07-16 19:07:14 +0100

    fix to point at correct docmosis template
    Change-Id: I7a4027972145a95e03159a9b9d44ff16b3be4bb3

--- hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/nows/service/NowsTemplateRegistrationService.java
+++ hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/nows/service/NowsTemplateRegistrationService.java
@@ -1,5 +1,18 @@
 package uk.gov.moj.cpp.hearing.event.nows.service;
 
+import uk.gov.justice.services.common.configuration.Value;
+import uk.gov.moj.cpp.hearing.event.nows.service.exception.TemplateRegistrationException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
 import com.docmosis.SystemManager;
 import com.docmosis.document.converter.ConversionException;
 import com.docmosis.document.converter.NoConvertersRunningException;
@@ -12,17 +25,6 @@ import com.docmosis.template.store.TemplateStoreFactory;
 import com.docmosis.util.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.gov.justice.services.common.configuration.Value;
-import uk.gov.moj.cpp.hearing.event.nows.service.exception.TemplateRegistrationException;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.inject.Inject;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 @Startup
 @Singleton
@@ -48,7 +50,7 @@ public class NowsTemplateRegistrationService {
             final TemplateContext templateContext = new TemplateContext(TEMPLATE_CONTEXT);
             final TemplateStore store = TemplateStoreFactory.getStore();
             final TemplateIdentifier templateId = new TemplateIdentifier(TEMPLATE_IDENTIFIER, templateContext);
-            StoreHelper.storeTemplate(templateId, getClass().getClassLoader().getResourceAsStream("templates/NoticeOrderWarrantsTemplate_archived.docx"), true, true, store);
+            StoreHelper.storeTemplate(templateId, getClass().getClassLoader().getResourceAsStream("templates/NoticeOrderWarrantsTemplate.docx"), true, true, store);
         } catch (IOException | TemplateStoreException | ConversionException | NoConvertersRunningException e) {
             LOGGER.error("Error registering the Notice orders and warrants Results Template with Docmosis Service", e);
             throw new TemplateRegistrationException(e);
