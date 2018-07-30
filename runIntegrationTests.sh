54c75f628c9d885596f9b3a51fed17cdec5d2787
robindevilliers <robindevilliers@me.com>
2018-07-30 14:31:31 +0100

    upgraded service parent pom version
    Change-Id: Ic8011ebcc28b96695cc55aa449ca317606bfd6a9

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -14,8 +14,8 @@ fi
 
 declare -rx WILDFLY_DEPLOYMENT_DIR="${VAGRANT_DIR}/deployments"
 declare -rx CONTEXT_NAME=hearing
-declare -rx FRAMEWORK_VERSION=4.0.3
-declare -rx EVENT_BUFFER_VERSION=4.0.3
+declare -rx FRAMEWORK_VERSION=4.1.2
+declare -rx EVENT_BUFFER_VERSION=4.1.2
 declare -rx FILE_SERVICE_VERSION=1.14.0
 
 #fail script on error
