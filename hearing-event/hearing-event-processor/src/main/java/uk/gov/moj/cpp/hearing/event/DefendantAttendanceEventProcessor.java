a1a35217ef350ee282f136f65c2793dfd4a497e5
sudhakarv.dv2 <sudhakar.vajravel@hmcts.net>
2018-10-28 17:18:55 +0000

    GPE-6729: Provide notification to FE when application/vnd.hearing.update-defendant-attendance-on-hearing-day+json is called
    Change-Id: I4048fa169856bb259b3e7633a9e9389d7016fc3a

---
+++ hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/DefendantAttendanceEventProcessor.java
@@ -0,0 +1,27 @@
+package uk.gov.moj.cpp.hearing.event;
+
+import static uk.gov.justice.services.core.annotation.Component.EVENT_PROCESSOR;
+
+import uk.gov.justice.services.core.annotation.Handles;
+import uk.gov.justice.services.core.annotation.ServiceComponent;
+import uk.gov.justice.services.core.enveloper.Enveloper;
+import uk.gov.justice.services.core.sender.Sender;
+import uk.gov.justice.services.messaging.JsonEnvelope;
+import javax.inject.Inject;
+
+@SuppressWarnings("WeakerAccess")
+@ServiceComponent(EVENT_PROCESSOR)
+public class DefendantAttendanceEventProcessor {
+
+    @Inject
+    private Enveloper enveloper;
+
+    @Inject
+    private Sender sender;
+
+    @Handles("hearing.defendant-attendance-updated")
+    public void publishPublicDefendantAttendanceUpdatedEvent(final JsonEnvelope event) {
+        this.sender.send(this.enveloper.withMetadataFrom(event, "public.hearing.defendant-attendance-updated").apply(event.payloadAsJsonObject()));
+    }
+
+}
--- hearing-integration-test/src/test/java/uk/gov/moj/cpp/hearing/it/DefendantAttendanceIT.java
+++ hearing-integration-test/src/test/java/uk/gov/moj/cpp/hearing/it/DefendantAttendanceIT.java
@@ -1,16 +1,19 @@
 package uk.gov.moj.cpp.hearing.it;
 
 import static org.hamcrest.Matchers.is;
+import static uk.gov.moj.cpp.hearing.it.Utilities.listenFor;
 import static uk.gov.moj.cpp.hearing.test.CommandHelpers.h;
 import static uk.gov.moj.cpp.hearing.test.TestTemplates.InitiateHearingCommandTemplates.standardInitiateHearingTemplate;
 import static uk.gov.moj.cpp.hearing.test.TestTemplates.UpdateDefendantAttendanceCommandTemplates.updateDefendantAttendanceTemplate;
 import static uk.gov.moj.cpp.hearing.test.matchers.BeanMatcher.isBean;
 import static uk.gov.moj.cpp.hearing.test.matchers.ElementAtListMatcher.first;
+import static uk.gov.moj.cpp.hearing.test.matchers.MapStringToTypeMatcher.convertStringTo;
 
 import org.junit.Test;
 import uk.gov.justice.json.schemas.core.AttendanceDay;
 import uk.gov.justice.json.schemas.core.DefendantAttendance;
 import uk.gov.justice.json.schemas.core.Hearing;
+import uk.gov.moj.cpp.hearing.domain.event.DefendantAttendanceUpdated;
 import uk.gov.moj.cpp.hearing.query.view.response.hearingresponse.HearingDetailsResponse;
 import uk.gov.moj.cpp.hearing.test.CommandHelpers;
 import java.time.LocalDate;
@@ -28,7 +31,17 @@ public class DefendantAttendanceIT extends AbstractIT {
         final UUID defendantId = hearingOne.getHearing().getProsecutionCases().get(0).getDefendants().get(0).getId();
         final LocalDate dateOfAttendance = hearingOne.getHearing().getHearingDays().get(0).getSittingDay().toLocalDate();
 
+        final Utilities.EventListener publicDefendantAttendanceUpdated = listenFor("public.hearing.defendant-attendance-updated", 30000)
+                .withFilter(convertStringTo(DefendantAttendanceUpdated.class, isBean(DefendantAttendanceUpdated.class)
+                        .with(DefendantAttendanceUpdated::getHearingId, is(hearingId))
+                        .with(DefendantAttendanceUpdated::getDefendantId, is(defendantId))
+                        .with(DefendantAttendanceUpdated::getAttendanceDay, isBean(AttendanceDay.class)
+                                .with(AttendanceDay::getDay, is(dateOfAttendance))
+                                .with(AttendanceDay::getIsInAttendance, is(Boolean.TRUE)))));
+
         h(UseCases.updateDefendantAttendance(requestSpec, updateDefendantAttendanceTemplate(hearingId, defendantId, dateOfAttendance, Boolean.TRUE)));
+
+        publicDefendantAttendanceUpdated.waitFor();
 
         Queries.getHearingPollForMatch(hearingId, 30, isBean(HearingDetailsResponse.class)
                 .with(HearingDetailsResponse::getHearing, isBean(Hearing.class)
