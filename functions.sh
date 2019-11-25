e98fc92bc741ef36f4102687a07274f6aa572fbd
Shaun Francis <shaun@mapingo.co.uk>
2019-11-25 11:50:25 +0000

    Update event-store to 2.4.2
    Change-Id: Iaa54e4483b3df73a2d94b0062c5b3dee10f1bdb6

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 
 declare -rx CONTEXT_NAME=hearing
 declare -rx FRAMEWORK_VERSION=6.4.0
-declare -rx EVENT_STORE_VERSION=2.4.1
+declare -rx EVENT_STORE_VERSION=2.4.2
 declare -rx FILE_SERVICE_VERSION=1.17.12
 
 function buildWars {
