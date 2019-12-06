329cd1cea44b3c4184f901cf9d15f581c4249b64
Shaun Francis <shaun@mapingo.co.uk>
2019-12-06 18:09:49 +0000

    Update service parent pom to 9.4.5
    Change-Id: I6a940c7e03abf44435f1059b9d75dc036fa1dcf6

--- functions.sh
+++ functions.sh
@@ -2,7 +2,7 @@
 
 declare -rx CONTEXT_NAME=hearing
 declare -rx FRAMEWORK_VERSION=6.4.0
-declare -rx EVENT_STORE_VERSION=2.4.2
+declare -rx EVENT_STORE_VERSION=2.4.3
 declare -rx FILE_SERVICE_VERSION=1.17.12
 
 function buildWars {
