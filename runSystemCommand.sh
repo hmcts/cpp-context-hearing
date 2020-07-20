9a3d6da005e43564b7ec278ee9d7d8f15bf7876f
Mahesh Subramanian <mahesh.subramanian1@hmcts.net>
2020-07-20 15:52:24 +0100

    Fixing JMX command script
    Change-Id: I999316ab4cd0266fca464f1d6545ea7af79734f2

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.4.1
+FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=7.0.7
 CONTEXT_NAME="hearing"
 USER_NAME="admin"
 PASSWORD="admin"
@@ -32,7 +32,7 @@ echo "Framework System Command Client for '$CONTEXT_NAME' context"
 if [ ! -f "$JAR" ]; then
     echo "Downloading artifacts..."
     echo
-    mvn --quiet org.apache.maven.plugins:maven-dependency-plugin:3.0.1:copy -DoutputDirectory=target -Dartifact=uk.gov.justice:framework-jmx-command-client:${FRAMEWORK_JMX_COMMAND_CLIENT_VERSION}:jar
+    mvn --quiet org.apache.maven.plugins:maven-dependency-plugin:3.0.1:copy -DoutputDirectory=target -Dartifact=uk.gov.justice.services:framework-jmx-command-client:${FRAMEWORK_JMX_COMMAND_CLIENT_VERSION}:jar
 fi
 
 if [ -z "$1" ]; then
