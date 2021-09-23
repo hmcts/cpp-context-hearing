1f200372a86192964f3483680e01315c1aa5534a
Kinshuk Garai <kinshuk.garai1@hmcts.net>
2021-09-23 16:17:56 +0100

    DD-15847: Updated rules so that Probation Admin users can call hearing.initiate command.
    Change-Id: I2c8e65c431d8ac43cb83285d178ce1959c66fc56

--- hearing-command/hearing-command-api/src/test/java/uk/gov/moj/cpp/hearing/command/api/HearingCommandApiAccessControlTest.java
+++ hearing-command/hearing-command-api/src/test/java/uk/gov/moj/cpp/hearing/command/api/HearingCommandApiAccessControlTest.java
@@ -65,7 +65,7 @@ public class HearingCommandApiAccessControlTest extends BaseDroolsAccessControlT
     @Test
     public void shouldAllowAuthorisedUserToInitiateHearing() {
         final Action action = createActionFor(ACTION_NAME_INITIATE_HEARING);
-        given(this.userAndGroupProvider.isMemberOfAnyOfTheSuppliedGroups(action, "Listing Officers", "Court Clerks", "Legal Advisers", "Court Administrators", "Crown Court Admin", "System Users", "Court Associate"))
+        given(this.userAndGroupProvider.isMemberOfAnyOfTheSuppliedGroups(action, "Probation Admin", "Listing Officers", "Court Clerks", "Legal Advisers", "Court Administrators", "Crown Court Admin", "System Users", "Court Associate"))
                 .willReturn(true);
 
         final ExecutionResults results = executeRulesWith(action);
