a2c71028f123056866dabcd2f16751a3a286fd99
Baris Ozturkmen <baris.ozturkmen@hmcts.net>
2022-10-03 16:19:24 +0100

    added logging for public event public.hearing.draft-result-saved
    Change-Id: Ia0eae5544242a3a2b7f3b03d896c61a05f413354

--- hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/HearingEventProcessor.java
+++ hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/HearingEventProcessor.java
@@ -96,7 +96,9 @@ public class HearingEventProcessor {
 
         final JsonObject publicEventPayload = this.objectToJsonObjectConverter.convert(publicHearingDraftResultSaved);
 
+       if(LOGGER.isErrorEnabled())  {LOGGER.error("raising public event hearing.draft-result-saved correlationId: {}" , event.metadata().clientCorrelationId().orElse(null));}
         this.sender.send(envelopeFrom(metadataFrom(event.metadata()).withName(PUBLIC_HEARING_DRAFT_RESULT_SAVED), publicEventPayload));
+        if(LOGGER.isErrorEnabled()) {LOGGER.error("raised public event hearing.draft-result-saved correlationId: {}" , event.metadata().clientCorrelationId().orElse(null));}
     }
 
     @Handles("hearing.draft-result-saved-v2")
