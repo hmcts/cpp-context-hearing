0b9edfc371b9adc0f3c52b4660bb173e6e16e74d
Ozgur Yalcin <ozgur.yalcin@hmcts.net>
2023-01-17 17:00:23 +0000

    New logs have been added for cache mechanism.
    Change-Id: I88ae87b14b80de4ffd0604f3867a7f84e50e357c

--- hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/service/NowsReferenceCache.java
+++ hearing-event/hearing-event-processor/src/main/java/uk/gov/moj/cpp/hearing/event/service/NowsReferenceCache.java
@@ -1,6 +1,8 @@
 package uk.gov.moj.cpp.hearing.event.service;
 
 
+import static java.util.Objects.nonNull;
+
 import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.moj.cpp.hearing.event.helper.TreeNode;
 import uk.gov.moj.cpp.hearing.event.nowsdomain.referencedata.resultdefinition.AllFixedList;
@@ -54,8 +56,13 @@ public class NowsReferenceCache {
         try {
             this.context.set(context);
             final Map<UUID, TreeNode<ResultDefinition>> allResultDefinitions = (Map<UUID, TreeNode<ResultDefinition>>) cache.get(new CacheKey(Type.RESULT_DEFINITIONS, referenceDate));
-
-            return allResultDefinitions.get(resultDefinitionId);
+            TreeNode<ResultDefinition> resultDefinitionTreeNode = allResultDefinitions.get(resultDefinitionId);
+            if (nonNull(resultDefinitionTreeNode)) {
+                LOGGER.error("resultDefinitionId: {} found in the cache for date {}", resultDefinitionTreeNode.getId(), referenceDate);
+            } else {
+                LOGGER.error("resultDefinitionId: {} NOT found in the cache for date {}", resultDefinitionId, referenceDate);
+            }
+            return resultDefinitionTreeNode;
         } catch (final ExecutionException executionException) {
             LOGGER.error("getResultDefinitionById reference data service not available", executionException);
             throw new RuntimeException(UNRECOVERABLE_SYSTEM_ERROR, executionException);
