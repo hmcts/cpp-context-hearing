7ef2c6fed4a5882c965a7dd6a6c8b176ea165187
gunnertwin <daniel@alvestechnology.io>
2024-02-22 15:48:53 +0000

    Update liquibase script to use dynamic dbPort
    Change-Id: Iede741cc17fe32aa524d5203fe84555a3c20d028

--- docker/scripts/liquibase.sh
+++ docker/scripts/liquibase.sh
@@ -2,49 +2,50 @@ echo "Running Liquibase"
 dbServerName=$1
 dbUserName=$2
 dbPassword=$3
-java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingeventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+dbPort=${5:-5432}
+java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingeventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingeventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingeventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar hearing-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar hearing-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingsystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingsystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar activiti-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/hearingactiviti?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar activiti-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/hearingactiviti?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
