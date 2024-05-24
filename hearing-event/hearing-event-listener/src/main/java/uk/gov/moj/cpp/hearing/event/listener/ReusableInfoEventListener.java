71c292bf9bc1b0241718e3c90c0feffd4a94157b
Zeeshan Alam <zeeshan.alam@hmcts.net>
2024-05-24 10:28:31 +0100

    SNI-3636 : Bail Conditions not Pulling Through Change-Id: I8720c981e6882907a8296ef3b0a4cd176c522b2b

--- hearing-event/hearing-event-listener/src/main/java/uk/gov/moj/cpp/hearing/event/listener/ReusableInfoEventListener.java
+++ hearing-event/hearing-event-listener/src/main/java/uk/gov/moj/cpp/hearing/event/listener/ReusableInfoEventListener.java
@@ -1,5 +1,6 @@
 package uk.gov.moj.cpp.hearing.event.listener;
 
+import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toSet;
 import static javax.json.Json.createArrayBuilder;
@@ -17,18 +18,31 @@ import uk.gov.moj.cpp.hearing.domain.event.ReusableInfoSaved;
 import uk.gov.moj.cpp.hearing.repository.ReusableInfoRepository;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ServiceComponent(EVENT_LISTENER)
 public class ReusableInfoEventListener {
 
+    public static final String PROMPT_REF = "promptRef";
+    public static final String TYPE = "type";
+    public static final String MASTER_DEFENDANT_ID = "masterDefendantId";
+    public static final String VALUE = "value";
+    public static final String OFFENCE_ID = "offenceId";
+    public static final String CACHEABLE = "cacheable";
+    public static final String CACHE_DATA_PATH = "cacheDataPath";
+    public static final String SHORT_CODE = "shortCode";
     private final ObjectMapper mapper = new ObjectMapperProducer().objectMapper();
 
     @Inject
@@ -38,22 +52,28 @@ public class ReusableInfoEventListener {
     public void saveReusableInfo(final Envelope<ReusableInfoSaved> event) {
         final ReusableInfoSaved reusableInfoSaved = event.payload();
 
+
         final Set<UUID> defendantIds = reusableInfoSaved.getPromptList().stream().map(ReusableInfo::getMasterDefendantId).collect(toSet());
         defendantIds.addAll(reusableInfoSaved.getResultsList().stream().map(ReusableInfoResults::getMasterDefendantId).collect(toSet()));
 
+        final List<uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo> existingReusableInfos = reusableInfoRepository.findReusableInfoByMasterDefendantIds(new ArrayList<>(defendantIds));
         for (final UUID defendantId : defendantIds) {
+
+            final List<ReusableInfo> existingReusablePromptsForDefendant = getExistingReusablePrompts(defendantId, existingReusableInfos);
+            final List<ReusableInfoResults> existingReusableResultsForDefendant = getExistingReusableResults(defendantId, existingReusableInfos);
+
             final JsonObjectBuilder cacheBuilder = createObjectBuilder();
-            final JsonArrayBuilder reusablePromptsBuilder = createArrayBuilder();
-            final JsonArrayBuilder reusableResultsBuilder = createArrayBuilder();
+            final JsonArrayBuilder newReusablePromptsBuilder = createArrayBuilder();
+            final JsonArrayBuilder newReusableResultsBuilder = createArrayBuilder();
 
             if(isNotEmpty(reusableInfoSaved.getPromptList())){
-                populateReusablePrompts(reusableInfoSaved, defendantId, reusablePromptsBuilder);
-                cacheBuilder.add("reusablePrompts", reusablePromptsBuilder.build());
+                populateReusablePrompts(reusableInfoSaved, defendantId, newReusablePromptsBuilder, existingReusablePromptsForDefendant);
+                cacheBuilder.add("reusablePrompts", newReusablePromptsBuilder.build());
             }
 
             if(isNotEmpty(reusableInfoSaved.getResultsList())){
-                populateReusableResults(reusableInfoSaved, defendantId, reusableResultsBuilder);
-                cacheBuilder.add("reusableResults", reusableResultsBuilder.build());
+                populateReusableResults(reusableInfoSaved, defendantId, newReusableResultsBuilder, existingReusableResultsForDefendant);
+                cacheBuilder.add("reusableResults", newReusableResultsBuilder.build());
             }
 
             final uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo reusableInfo = new uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo(defendantId, mapper.valueToTree(cacheBuilder.build()), ZonedDateTime.now());
@@ -61,31 +81,110 @@ public class ReusableInfoEventListener {
         }
     }
 
-    private void populateReusableResults(final ReusableInfoSaved reusableInfoSaved, final UUID defendantId, final JsonArrayBuilder resultsArrBuilder) {
-        reusableInfoSaved.getResultsList().stream()
-                .filter(resultsCache -> defendantId.equals(resultsCache.getMasterDefendantId()))
-                .map(resultsCache -> createObjectBuilder()
-                        .add("masterDefendantId", resultsCache.getMasterDefendantId().toString())
-                        .add("offenceId", resultsCache.getOffenceId().toString())
-                        .add("shortCode",resultsCache.getShortCode())
-                        .add("value", resultsCache.getValue()))
-                .forEach(resultsArrBuilder::add);
+    private  List<ReusableInfo> getExistingReusablePrompts(final UUID defendantId, final List<uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo> existingReusableInfos) {
+        final Optional<uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo> reusableInfo = existingReusableInfos.stream().filter(re -> re.getId().equals(defendantId)).findFirst();
+        final List<JsonNode> list = new ArrayList();
+        reusableInfo.ifPresent(re -> {
+            final JsonNode reusablePrompts = re.getPayload().get("reusablePrompts");
+            if (nonNull(reusablePrompts) && reusablePrompts.isArray()) {
+                reusablePrompts.forEach(list::add);
+            }
+        });
+
+        return list.stream().map(e -> {
+                            final ReusableInfo.Builder reusableInfoBuilder = ReusableInfo.builder()
+                            .withPromptRef(e.get(PROMPT_REF).asText())
+                            .withMasterDefendantId(UUID.fromString(e.get(MASTER_DEFENDANT_ID).asText()))
+                            .withType(e.get(TYPE).asText())
+                            .withValue(e.get(VALUE).asText())
+                            .withOffenceId(UUID.fromString(e.get(OFFENCE_ID).asText()));
+                            ofNullable(e.get(CACHEABLE)).ifPresent(cacheable -> reusableInfoBuilder.withCacheable(e.get(CACHEABLE).asInt()));
+                            ofNullable(e.get(CACHE_DATA_PATH)).ifPresent(cacheable -> reusableInfoBuilder.withCacheDataPath(e.get(CACHE_DATA_PATH).asText()));
+                            return reusableInfoBuilder.build();
+                }
+        ).collect(Collectors.toList());
+
     }
 
-    private void populateReusablePrompts(final ReusableInfoSaved reusableInfoSaved, final UUID defendantId, final JsonArrayBuilder promptsArrBuilder) {
+    private  List<ReusableInfoResults> getExistingReusableResults(final UUID defendantId, final List<uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo> existingReusableInfos) {
+        final Optional<uk.gov.moj.cpp.hearing.persist.entity.ha.ReusableInfo> reusableInfo = existingReusableInfos.stream().filter(re -> re.getId().equals(defendantId)).findFirst();
+        final List<JsonNode> list = new ArrayList();
+        reusableInfo.ifPresent(re -> {
+            final JsonNode reusableResults = re.getPayload().get("reusableResults");
+            if (nonNull(reusableResults) && reusableResults.isArray()) {
+                reusableResults.forEach(list::add);
+            }
+        });
+
+        return list.stream().map(e -> {
+                    final ReusableInfoResults.Builder reusableInfoBuilder = ReusableInfoResults.builder()
+                            .withMasterDefendantId(UUID.fromString(e.get(MASTER_DEFENDANT_ID).asText()))
+                            .withShortCode(e.get(SHORT_CODE).asText())
+                            .withValue(e.get(VALUE).asText())
+                            .withOffenceId(UUID.fromString(e.get(OFFENCE_ID).asText()));
+                    return reusableInfoBuilder.build();
+                }
+        ).collect(Collectors.toList());
+
+    }
+
+    private void populateReusableResults(final ReusableInfoSaved reusableInfoSaved, final UUID defendantId, final JsonArrayBuilder resultsArrBuilder, final List<ReusableInfoResults> existingReusableResultsForDefendant) {
+        final List<UUID> inputOffenceListForDefendant  = new ArrayList();
+        reusableInfoSaved.getResultsList().stream()
+                .filter(resultsCache -> defendantId.equals(resultsCache.getMasterDefendantId()))
+                .map(resultsCache -> {
+                    final JsonObjectBuilder resultBuilder = createObjectBuilder()
+                            .add(MASTER_DEFENDANT_ID, resultsCache.getMasterDefendantId().toString())
+                            .add(OFFENCE_ID, resultsCache.getOffenceId().toString())
+                            .add(SHORT_CODE,resultsCache.getShortCode())
+                            .add(VALUE, resultsCache.getValue());
+                    inputOffenceListForDefendant.add(resultsCache.getOffenceId());
+                    return resultBuilder;
+                })
+                .forEach(resultsArrBuilder::add);
+        existingReusableResultsForDefendant.stream()
+                .filter(e -> !inputOffenceListForDefendant.contains(e.getOffenceId()))
+                .forEach(re-> {
+                    final JsonObjectBuilder resultBuilder = createObjectBuilder()
+                            .add(MASTER_DEFENDANT_ID, re.getMasterDefendantId().toString())
+                            .add(OFFENCE_ID, re.getOffenceId().toString())
+                            .add(SHORT_CODE, re.getShortCode())
+                            .add(VALUE, re.getValue());
+
+                    resultsArrBuilder.add(resultBuilder);
+                });
+    }
+
+    private void populateReusablePrompts(final ReusableInfoSaved reusableInfoSaved, final UUID defendantId, final JsonArrayBuilder promptsArrBuilder, final List<ReusableInfo> existingReusablePromptsForDefendant) {
+        final List<UUID> inputOffenceListForDefendant  = new ArrayList();
         reusableInfoSaved.getPromptList().stream()
                 .filter(prompt -> defendantId.equals(prompt.getMasterDefendantId()))
                 .map(prompt -> {
                     final JsonObjectBuilder promptBuilder = createObjectBuilder()
-                            .add("promptRef", prompt.getPromptRef())
-                            .add("masterDefendantId", prompt.getMasterDefendantId().toString())
-                            .add("offenceId", prompt.getOffenceId().toString())
-                            .add("type", prompt.getType())
-                            .add("value", prompt.getValue().toString());
-                    ofNullable(prompt.getCacheable()).ifPresent(cacheable -> promptBuilder.add("cacheable", cacheable));
-                    ofNullable(prompt.getCacheDataPath()).ifPresent(cacheDataPath -> promptBuilder.add("cacheDataPath", cacheDataPath));
+                            .add(PROMPT_REF, prompt.getPromptRef())
+                            .add(MASTER_DEFENDANT_ID, prompt.getMasterDefendantId().toString())
+                            .add(OFFENCE_ID, prompt.getOffenceId().toString())
+                            .add(TYPE, prompt.getType())
+                            .add(VALUE, prompt.getValue().toString());
+                    ofNullable(prompt.getCacheable()).ifPresent(cacheable -> promptBuilder.add(CACHEABLE, cacheable));
+                    ofNullable(prompt.getCacheDataPath()).ifPresent(cacheDataPath -> promptBuilder.add(CACHE_DATA_PATH, cacheDataPath));
+                    inputOffenceListForDefendant.add(prompt.getOffenceId());
                     return promptBuilder;
                 })
                 .forEach(promptsArrBuilder::add);
+
+        existingReusablePromptsForDefendant.stream()
+                .filter(e -> !inputOffenceListForDefendant.contains(e.getOffenceId()))
+                .forEach(re-> {
+                        final JsonObjectBuilder promptBuilder = createObjectBuilder()
+                        .add(PROMPT_REF, re.getPromptRef())
+                        .add(MASTER_DEFENDANT_ID, re.getMasterDefendantId().toString())
+                        .add(OFFENCE_ID, re.getOffenceId().toString())
+                        .add(TYPE, re.getType())
+                        .add(VALUE, re.getValue().toString());
+                        ofNullable(re.getCacheable()).ifPresent(cacheable -> promptBuilder.add(CACHEABLE, cacheable));
+                        ofNullable(re.getCacheDataPath()).ifPresent(cacheDataPath -> promptBuilder.add(CACHE_DATA_PATH, cacheDataPath));
+                        promptsArrBuilder.add(promptBuilder);
+                });
     }
 }
