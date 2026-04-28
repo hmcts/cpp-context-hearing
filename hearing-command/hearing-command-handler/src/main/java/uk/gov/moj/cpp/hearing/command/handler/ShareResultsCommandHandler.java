3f919e857532d74d6dbaa872d4f18cbf9d7b5f25
MohamedFarouk-HMCTS <mohamed.farouk@hmcts.net>
2026-04-28 18:06:10 +0100

    Fix sonar issue

--- hearing-command/hearing-command-handler/src/main/java/uk/gov/moj/cpp/hearing/command/handler/ShareResultsCommandHandler.java
+++ hearing-command/hearing-command-handler/src/main/java/uk/gov/moj/cpp/hearing/command/handler/ShareResultsCommandHandler.java
@@ -40,7 +40,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
